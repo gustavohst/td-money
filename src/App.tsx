63ffcc6b2847b7f04c53a835915fc4fbe6723b4e
Gustavo Tenório <gustavohst@msn.com>
2022-02-13 17:35:30 +0000

    Create summary and Dashbord components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Dashboard } from "./componentes/Dashbord";
 import { Header } from "./componentes/Header";
+
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyle/>
     </>
   );
---
+++ src/componentes/Summary/index.tsx
@@ -0,0 +1,32 @@
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import totalImg from '../../assets/total.svg'
+import { Container } from "./styles";
+
+export function Summary() {
+    return(
+        <Container>
+            <section>
+                <header>
+                    <p>Income</p>
+                    <img src={incomeImg} alt="Income" />
+                </header>
+                <strong>R$1000,00</strong>
+            </section>
+            <section>
+                <header>
+                    <p>Outcome</p>
+                    <img src={outcomeImg} alt="Outcome" />
+                </header>
+                <strong> - R$500,00</strong>
+            </section>
+            <section className="highlight-background">
+                <header>
+                    <p>Total</p>
+                    <img src={totalImg} alt="Total" />
+                </header>
+                <strong>R$500,00</strong>
+            </section>
+        </Container>
+    );
+}
